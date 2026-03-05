6f985c05f71471b59f1914aed9442595801ffe91
jonahmer22 <jonah@jonahmerriam.net>
2026-03-05 13:20:26 -0500

    Removed Depricated Functionality & Yaml from ann
    Removed depricated functionality `Announcement_prefix_path_attributes_eq` and `Announcement_bgpsec_valid`. Also removed Yaml functionality `Announcement_to_yaml_dict` and `Announcement_from_yaml_dict`. No need for depricated functions in new version.

--- bgpy/simulation_engine/announcement.py
+++ bgpy/simulation_engine/announcement.py
@@ -1,9 +1,6 @@
-from yamlable import YamlAble, yaml_info
-
 # import the C extension
 from ._announcement_c import Announcement as _CAnnouncement
 
-# create a class from the C extension portion of the code and the C extension
-@yaml_info(yaml_tag="Announcement")
-class Announcement(_CAnnouncement, YamlAble):
+# create a class from the C extension
+class Announcement(_CAnnouncement):
     """BGP Announcement"""
